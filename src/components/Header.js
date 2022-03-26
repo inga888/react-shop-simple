3e20228938bc015e71a19f9a2d7dc4d538bb8b39
Inga Zhamsoeva <injoyme@gmail.com>
2022-03-26 15:43:08 +0300

    add links

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ const Header = () => {
   return (
     <nav className="deep-purple">
     <div className="nav-wrapper">
-      <a href="#!" className="brand-logo">React Shop</a>
+      <a href="/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="#!">Repo</a></li>
+        <li><a href="https://github.com/inga888/reactshop_simple"
+        target="_blank"
+        rel="noreferrer"
+        >Repo</a></li>
       </ul>
     </div>
   </nav>
