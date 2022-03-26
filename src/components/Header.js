0ab931bfca8dc8bc5eae9420123a94b2c2bf9d39
Inga Zhamsoeva <injoyme@gmail.com>
2022-03-26 16:00:25 +0300

    correct links

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     <div className="nav-wrapper">
       <a href="/" className="brand-logo">React Shop</a>
       <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="https://github.com/inga888/reactshop_simple"
+        <li><a href="https://github.com/inga888/react-shop-simple"
         target="_blank"
         rel="noreferrer"
         >Repo</a></li>
