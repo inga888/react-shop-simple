665e5a7fa6b1d5f215d6ece4c0cc9d97d2499a8c
Inga Zhamsoeva <injoyme@gmail.com>
2022-03-26 15:55:38 +0300

    add link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
           <div className="footer-copyright">
             <div className="container">
             © {new Date().getFullYear()} Copyright Text
-            <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+            <a className="grey-text text-lighten-4 right" href="https://github.com/inga888/react-shop-simple" target="_blank" rel="noreferrer">Repo</a>
             </div>
           </div>
         </footer>
